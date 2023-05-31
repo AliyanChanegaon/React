5af83c37184bf1afaf4f70c8edd014e0721677cc
Aliyan07 <chanegaonaliyan7@gmail.com>
2023-05-31 15:52:41 +0530

    -

--- todo-json-server/frontend/src/components/todo.jsx
+++ todo-json-server/frontend/src/components/todo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import EditIcon from '@mui/icons-material';
+import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SaveIcon from '@mui/icons-material/Save';
 
@@ -14,7 +14,6 @@ export const Todo = ({ data, ChangeStatus, DeleteTodo , editTodo }) => {
       <p>{data.status ? "Done" : "Pending"}</p>
       <button onClick={statusChange}>Change</button>
       <button style={{backgroundColor:"red",color:"white"}} onClick={() => DeleteTodo(data.id)}><DeleteIcon/></button>
-
       <button style={{backgroundColor:"blue",color:"white"}} onClick={() => editTodo(data.id)} ><EditIcon /></button>
       <button style={{backgroundColor:"green",color:"white"}}><SaveIcon/></button>
     </div>
