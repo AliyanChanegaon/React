cb276c015f79ae28aa33734ff9b3c12e5ac33339
Aliyan07 <chanegaonaliyan7@gmail.com>
2023-06-06 19:38:15 +0530

    displayed data User wiser

--- Routing/src/component/pages/SingleUser.jsx
+++ Routing/src/component/pages/SingleUser.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 export default function SingleUser() {
   const { id } = useParams();
-  console.log(id);
+  const [data, setData] = useState({});
+
+  const getUsers = (id) => {
+    return fetch(`https://reqres.in/api/users/${id}`).then((res) => res.json());
+  };
+  useEffect(() => {
+    getUsers(id).then((res) => {
+      setData(res);
+      console.log(res);
+    });
+  }, [id]);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <img src={data?.data?.avatar} alt="" />
+      <div style={{ display: "flex", justifyContent: "center", gap: "5px" }}>
+        <p>{data?.data?.first_name}</p>
+        <p>{data?.data?.last_name}</p>
+      </div>
+
+      <h1>{data?.data?.email}</h1>
     </div>
   );
 }
