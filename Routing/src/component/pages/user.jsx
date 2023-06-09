fe01568471fd765b2b0b88635f1d12d06759fa7e
Aliyan07 <chanegaonaliyan7@gmail.com>
2023-06-09 15:19:22 +0530

    Routing changing url realtime

--- Routing/src/component/pages/user.jsx
+++ Routing/src/component/pages/user.jsx
@@ -1,27 +1,37 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
-function getUsers() {
+export function getUsers() {
   return fetch("https://reqres.in/api/users").then((res) => res.json());
 }
 
 export default function User() {
+  let [searchParams, setSearchParams] = useSearchParams();
   const [data, setData] = useState({});
+  const [inputValue, setInputValue] = useState("");
+  const [text, setText] = useState(searchParams.get("q") || "");
   useEffect(() => {
     getUsers().then((res) => {
       console.log(res);
       setData(res);
     });
-  }, []);
+  }, [inputValue]);
+  useEffect(() => {
+    setSearchParams({ q: text });
+  }, [text, setSearchParams]);
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+  };
 
   return (
     <div>
       <h1>User</h1>
+      <input type="text" placeholder="Enter" onChange={handleInputChange} />
       <div>
         {data?.data?.map((item) => {
           return (
             <div>
-              <Link to={`/user/${item.id}`} key={item.id}>
+              <Link to={`/user/${item.id}/${inputValue}`} key={item.id}>
                 {item.first_name}
               </Link>
             </div>
